e132862e493d97eb54392e1c3bbc47f61f585fcd
Minto <xpeed96@gmail.com>
2023-08-07 14:02:04 +0900

    [Feat]: 할일 input box maxLength 속성 추가

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
       <h1 className={styles.title}>ToDoList</h1>
       <main className={styles.main}>
         <div className={styles.search}>
-          <OutlinedInput className={styles.textField} type="text" placeholder="내용을 입력한 후, 오른쪽에 [할 일 추가]를 클릭해 주세요."/>
+          <OutlinedInput className={styles.textField} type="text" placeholder="내용을 입력한 후, 오른쪽에 [할 일 추가]를 클릭해 주세요." inputProps={{maxLength: 50}}/>
           <Button className={styles.addButton} variant="outlined">할 일 추가</Button>
         </div>
       </main>
