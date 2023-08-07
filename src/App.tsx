912bfe0ae4675ca9d78ebbca071363df54da0950
Minto <xpeed96@gmail.com>
2023-08-07 14:58:52 +0900

    [Feat]: App 컴포넌트에 추가 todoList HTML 작성

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Button from '@mui/material/Button';
 import { OutlinedInput } from '@mui/material';
 import styles from './app.module.scss';
+import TodoItem from './components/todoItem/TodoItem';
 
 function App() {
 
@@ -8,10 +9,15 @@ function App() {
     <div className={styles.container}>
       <h1 className={styles.title}>ToDoList</h1>
       <main className={styles.main}>
-        <div className={styles.search}>
+        <section className={styles.search}>
           <OutlinedInput className={styles.textField} type="text" placeholder="내용을 입력한 후, 오른쪽에 [할 일 추가]를 클릭해 주세요." inputProps={{maxLength: 50}}/>
           <Button className={styles.addButton} variant="outlined">할 일 추가</Button>
-        </div>
+        </section>
+        <section className={styles.todoList}>
+          <TodoItem />
+          <TodoItem />
+          <TodoItem />
+        </section>
       </main>
     </div>
   )
