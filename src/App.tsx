a916d27a1e0d80fd454bc9df27847d6a55c28b5c
Minto <xpeed96@gmail.com>
2023-08-07 13:54:07 +0900

    [Feat]: app 컴포넌트 작성 및 스타일링

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Button from '@mui/material/Button';
+import { OutlinedInput } from '@mui/material';
+import styles from './app.module.scss';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className={styles.container}>
+      <h1 className={styles.title}>ToDoList</h1>
+      <main className={styles.main}>
+        <div className={styles.search}>
+          <OutlinedInput className={styles.textField} type="text" placeholder="내용을 입력한 후, 오른쪽에 [할 일 추가]를 클릭해 주세요."/>
+          <Button className={styles.addButton} variant="outlined">할 일 추가</Button>
+        </div>
+      </main>
+    </div>
   )
 }
 
