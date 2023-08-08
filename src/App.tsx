7ee38900092567ca1213abaf8f181c8fba698656
Minto <xpeed96@gmail.com>
2023-08-08 15:22:10 +0900

    [Feat]: 입력한 todo content 서버에 post 로직 처리 및 type 작성

--- src/App.tsx
+++ src/App.tsx
@@ -5,8 +5,24 @@ import styles from './app.module.scss';
 import TodoItem from './components/todoItem/TodoItem';
 import instance from './api/instance';
 
+export type TodoItem = {
+  id: number;
+  content: string;
+  isCompleted: boolean;
+  createdDateTime: string;
+  updatedDateTime: string;
+};
+
+export type TodoList = {
+  value: TodoItem[];
+  count: number;
+  offset: number;
+  limit: number;
+  total: number;
+};
+
 export default function App() {
-  const [todoList, setTodoList] = useState([]);
+  const [todoList, setTodoList] = useState<TodoItem[]>([]);
   const contentRef = useRef();
 
   const getTodoList = async () => {
@@ -22,6 +38,19 @@ export default function App() {
     getTodoList();
   }, []);
 
+  const handleAddTodoList = async () => {
+    const todoValue = contentRef.current?.value;
+    try {
+      const { data } = await instance.post('/todos', {
+        content: todoValue
+      });
+
+      setTodoList([...todoList, data]);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>ToDoList</h1>
@@ -34,7 +63,7 @@ export default function App() {
             placeholder="내용을 입력한 후, 오른쪽에 [할 일 추가]를 클릭해 주세요."
             inputProps={{ maxLength: 50 }}
           />
-          <Button className={styles.addButton} variant="outlined">
+          <Button className={styles.addButton} variant="outlined" onClick={handleAddTodoList}>
             할 일 추가
           </Button>
         </section>
