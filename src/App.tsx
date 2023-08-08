9169e6a22d0301a235a7fa792e943d2f72f65c3e
Minto <xpeed96@gmail.com>
2023-08-08 15:02:05 +0900

    [Feat]: 초기 컴포넌트 마운트시 서버로부터 todolist 받아오는 로직 작성

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,49 @@
+import { useEffect, useRef, useState } from 'react';
 import Button from '@mui/material/Button';
 import { OutlinedInput } from '@mui/material';
 import styles from './app.module.scss';
 import TodoItem from './components/todoItem/TodoItem';
+import instance from './api/instance';
 
-function App() {
+export default function App() {
+  const [todoList, setTodoList] = useState([]);
+  const contentRef = useRef();
+
+  const getTodoList = async () => {
+    try {
+      const { data } = await instance.get('/todos?offset=0&limit=50');
+      setTodoList(data.value);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getTodoList();
+  }, []);
 
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>ToDoList</h1>
       <main className={styles.main}>
         <section className={styles.search}>
-          <OutlinedInput className={styles.textField} type="text" placeholder="내용을 입력한 후, 오른쪽에 [할 일 추가]를 클릭해 주세요." inputProps={{maxLength: 50}}/>
-          <Button className={styles.addButton} variant="outlined">할 일 추가</Button>
+          <OutlinedInput
+            inputRef={contentRef}
+            className={styles.textField}
+            type="text"
+            placeholder="내용을 입력한 후, 오른쪽에 [할 일 추가]를 클릭해 주세요."
+            inputProps={{ maxLength: 50 }}
+          />
+          <Button className={styles.addButton} variant="outlined">
+            할 일 추가
+          </Button>
         </section>
         <section className={styles.todoList}>
-          <TodoItem />
-          <TodoItem />
-          <TodoItem />
+          {todoList.map((item) => (
+            <TodoItem todoItem={item} />
+          ))}
         </section>
       </main>
     </div>
-  )
+  );
 }
-
-export default App
--- src/components/todoItem/TodoItem.tsx
+++ src/components/todoItem/TodoItem.tsx
@@ -3,13 +3,23 @@ import { Typography } from '@mui/material';
 import Button from '@mui/material/Button';
 import styles from './todoItem.module.scss';
 
-export default function TodoItem() {
-  
-  return <article className={styles.container}>
-    <Checkbox />
-    <Typography className={styles.content}>여기 할일이 표기됩니다.1111111111111111111111</Typography>
-    <Button className={styles.changeButton} variant="outlined">수정</Button>
-    <Button className={styles.deleteButton} variant="outlined">삭제</Button>
-  </article>;
+export type Props = {
+  todoItem: {
+    content: string;
+  };
+};
 
+export default function TodoItem({ todoItem }: Props) {
+  return (
+    <article className={styles.container}>
+      <Checkbox />
+      <Typography className={styles.content}>{todoItem.content}</Typography>
+      <Button className={styles.changeButton} variant="outlined">
+        수정
+      </Button>
+      <Button className={styles.deleteButton} variant="outlined">
+        삭제
+      </Button>
+    </article>
+  );
 }
