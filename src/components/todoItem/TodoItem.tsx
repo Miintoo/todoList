534e9b92f9c8b89f59bfc24fce7b6675f1d88248
Minto <xpeed96@gmail.com>
2023-08-08 20:05:07 +0900

    [Style]: 기존 TodoItem 수정모드에 적용 가능한 로직 추가

--- src/components/todoItem/TodoItem.tsx
+++ src/components/todoItem/TodoItem.tsx
@@ -2,27 +2,26 @@ import Checkbox from '@mui/material/Checkbox';
 import { Typography } from '@mui/material';
 import Button from '@mui/material/Button';
 import styles from './todoItem.module.scss';
-import { TodoItem } from '../../App';
 import instance from '../../api/instance';
 import { useState } from 'react';
+import { TodoProps } from '../../container/TodoListContainer';
 
-type TodoProps = {
-  item: TodoItem;
-  onDeleteTodoList: (id: number) => void;
-};
-
-export default function TodoItemComponent({ item, onDeleteTodoList }: TodoProps) {
+export default function TodoItemComponent({ item, onDeleteTodoList, onChangeMode }: TodoProps) {
   const [checked, setChecked] = useState(false);
   const handleDeleteTodoItem = async () => {
     const isDeleted = confirm('할 일을 삭제하시겠습니까? \n삭제한 할 일은 복구할 수 없습니다.');
     if (isDeleted) {
       try {
         await instance.delete(`/todos/${item.id}`);
-        onDeleteTodoList(item.id);
+        onDeleteTodoList?.(item.id);
       } catch (error) {
         console.log(error);
       }
     }
+  };
+
+  const handleChangeMode = () => {
+    onChangeMode?.();
   };
 
   const handleCheckboxChange = () => {
@@ -39,7 +38,7 @@ export default function TodoItemComponent({ item, onDeleteTodoList }: TodoProps)
       >
         {item.content}
       </Typography>
-      <Button className={styles.changeButton} variant="outlined">
+      <Button className={styles.changeButton} variant="outlined" onClick={handleChangeMode}>
         수정
       </Button>
       <Button className={styles.deleteButton} variant="outlined" onClick={handleDeleteTodoItem}>
