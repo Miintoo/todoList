8574751ad8bb3fc9744ab5b1db2c89377f70ac21
Minto <xpeed96@gmail.com>
2023-08-09 21:47:21 +0900

    [Refactor]: 기존 api 로직 삭제 및 외부 로직 import

--- src/components/todoItem/TodoItem.tsx
+++ src/components/todoItem/TodoItem.tsx
@@ -2,22 +2,13 @@ import Checkbox from '@mui/material/Checkbox';
 import { Typography } from '@mui/material';
 import Button from '@mui/material/Button';
 import styles from './todoItem.module.scss';
-import instance from '../../api/instance';
 import { useState } from 'react';
-import { TodoProps } from '../../container/TodoListContainer';
-
+import { TodoProps } from '../../types/types';
+import { deleteTodoItem } from '../../api/api';
 export default function TodoItemComponent({ item, onDeleteTodoList, onChangeMode }: TodoProps) {
   const [checked, setChecked] = useState(item.isCompleted);
   const handleDeleteTodoItem = async () => {
-    const isDeleted = confirm('할 일을 삭제하시겠습니까? \n삭제한 할 일은 복구할 수 없습니다.');
-    if (isDeleted) {
-      try {
-        await instance.delete(`/todos/${item.id}`);
-        onDeleteTodoList?.(item.id);
-      } catch (error) {
-        console.log(error);
-      }
-    }
+    deleteTodoItem({ item, onDeleteTodoList });
   };
 
   const handleChangeMode = () => {
