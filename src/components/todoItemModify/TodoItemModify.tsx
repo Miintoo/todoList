b0bed77d3b6f9667bec8b3dea54493a0a917e34a
Minto <xpeed96@gmail.com>
2023-08-09 20:37:28 +0900

    [Refactor]: 기존 API 요청 로직 삭제

--- src/components/todoItemModify/TodoItemModify.tsx
+++ src/components/todoItemModify/TodoItemModify.tsx
@@ -3,9 +3,8 @@ import Checkbox from '@mui/material/Checkbox';
 import { OutlinedInput } from '@mui/material';
 import Button from '@mui/material/Button';
 import styles from './todoItemModify.module.scss';
-// import instance from '../../api/instance';
-import { TodoProps } from '../../container/TodoListContainer';
-import instance from '../../api/instance';
+import { TodoProps } from '../../types/types';
+import { patchTodoItem } from '../../api/api';
 
 export default function TodoItemModifyComponent({
   item,
@@ -14,27 +13,14 @@ export default function TodoItemModifyComponent({
   onHandleChangeContent
 }: TodoProps) {
   const [checked, setChecked] = useState(item.isCompleted);
-  const contentRef = useRef<HTMLInputElement>();
+  const contentRef = useRef<HTMLInputElement>(null);
 
   const handleCheckboxChange = () => {
     setChecked(!checked);
   };
 
-  const handleSaveContent = async () => {
-    const todoValue = contentRef.current?.value;
-
-    try {
-      const { data } = await instance.patch(`/todos/${item.id}`, {
-        content: todoValue,
-        isCompleted: checked
-      });
-
-      onChangeMode?.();
-      onHandleChangeContent?.(data.id, data.content, data.isCompleted);
-    } catch (error) {
-      alert('이미 삭제된 [할 일]입니다.');
-      onDeleteTodoList?.(item.id);
-    }
+  const handleSaveContent = () => {
+    patchTodoItem({ item, contentRef, checked, onChangeMode, onHandleChangeContent, onDeleteTodoList });
   };
 
   const handleChangeMode = () => {
