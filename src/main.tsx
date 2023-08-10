b8db41f86e6f94f8ef96fdd846bd7e7d2ff4d3ed
Minto <xpeed96@gmail.com>
2023-08-11 00:11:11 +0900

    [Feat]: main에 router 세팅

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
 import './styles/global.scss';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import ServerErrorPage from './pages/serverError/ServerErrorPage.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/serverError" element={<ServerErrorPage />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
